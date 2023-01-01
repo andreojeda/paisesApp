37d402b207fd3606a479122bcbe0b1a98315dff8
Andres Ojeda <carlosandresoj@gmail.com>
2022-12-31 20:10:31 -0500

    update por capital

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class VerPaisComponent {
 
+  pais!: Country;
+
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) {}
+
+  ngOnInit(): void {  
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.paisService.getPaisPorAlpha( id ) )
+    )
+    .subscribe( pais => {
+      console.log(pais);
+    });
+  }
 }
