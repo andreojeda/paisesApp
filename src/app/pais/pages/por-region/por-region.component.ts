6df128eb8e3f376c3d314c9baf68adef3b81063e
Andres Ojeda <carlosandresoj@gmail.com>
2023-01-11 01:19:25 -0500

    App End

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
@@ -6,13 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PorRegionComponent {
 
-    regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAAR']
+    regiones: string[] = [ "Africa", "Americas", "Asia", "Europe", "Oceania" ];
     regionActiva: string = '';
+    paises: Country[] = [];
 
-    
-    
-    
-    constructor() { }  
+    constructor( private paisService: PaisService ) { }
 
     getClaseCSS( region: string ): string {
       return (region === this.regionActiva )
@@ -21,7 +21,12 @@ export class PorRegionComponent {
     }
 
     activarRegion( region: string ) {
+
+      if ( region === this.regionActiva ) { return; }
+
       this.regionActiva = region;
+      this.paises = [];
+      this.paisService.buscarRegion( region ).subscribe( paises => this.paises = paises );
     }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
@@ -9,20 +9,25 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
+  private apiUrl2: string = 'https://restcountries.com/v2/';
+
+  get httpParams() {
+    return new HttpParams().set('fields','name,capital,cca2,flags,population,alpha2Code');
+  }
 
   constructor( private http: HttpClient) { }
 
   buscarPais( termino: string ): Observable<Country[]>{
-    
+
     const url = `${ this.apiUrl }/name/${ termino }`;
 
     return this.http.get<Country[]>( url );
   }
-  
+
   buscarCapital( termino: string ): Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url,{ params: this.httpParams }).pipe(tap(console.log));
   }
 
   getPaisPorAlpha( id: string ):Observable<Country[]>{
@@ -30,4 +35,11 @@ export class PaisService {
     return this.http.get<Country[]>( url );
 
   }
+  buscarRegion( region: string ):Observable<Country[]>{
+
+    const httpParams = new HttpParams();
+    const url = `${ this.apiUrl }/region/${ region }`;
+    return this.http.get<Country[]>( url, { params: this.httpParams }).pipe(tap(console.log));
+
+}
 }
